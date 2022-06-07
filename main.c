77421a863d5577ec0169dd5d03e70ffc86cc7976
Henrique Almeida <hcarvalhoalmeida7@gmail.com>
2022-06-07 00:31:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,66 +2,46 @@
 main()
 {
     FILE *pFile = fopen("mega_todos.csv", "r");
-    int m[6], i = 0, n[7], c, mod;
-    printf("\n |mega-sena: 0| |quina: 1| |quadra: 2|\n Digite sua modalidade: ");
-    scanf("%i", &mod);
+    int i[3] = {0, 0, 0}, n[7], m[6], c;
     if (pFile == '\0')
         printf("\n Erro fatal, o arquivo não pode ser aberto!\n");
     else
     {
-        if (mod == 0)
+        printf("\n Digite sua jogada:");
+        scanf("%i %i %i %i %i %i", &m[0], &m[1], &m[2], &m[3], &m[4], &m[5]);
+        do
         {
-            printf(" Digite sua jogada:");
-            scanf("%i %i %i %i %i %i", &m[0], &m[1], &m[2], &m[3], &m[4], &m[5]);
-            do
-            {
-                c = fscanf(pFile, "%i %i %i %i %i %i %i", &n[6], &n[0], &n[1], &n[2], &n[3], &n[4], &n[5]);
-                if (n[0] == m[0] && n[1] == m[1] && n[2] == m[2] && n[3] == m[3] && n[4] == m[4] && n[5] == m[5])
-                    i++;
-            } while (c != EOF);
-            if (i > 0)
-                printf("\n Voce ganhou a mega sena %i vezes!\n", i);
-            else
-                printf("\n Voce nao ganhou nenhum jogo\n");
-        }
-        else if (mod == 1)
+            c = fscanf(pFile, "%i %i %i %i %i %i %i", &n[6], &n[0], &n[1], &n[2], &n[3], &n[4], &n[5]);
+            if (n[0] == m[0] && n[1] == m[1] && n[2] == m[2] && n[3] == m[3] && n[4] == m[4] && n[5] == m[5])
+                i[0]++;
+            else if (n[0] == m[0] && n[1] == m[1] && n[2] == m[2] && n[3] == m[3] && n[4] == m[4])
+                i[1]++;
+            else if (n[1] == m[0] && n[2] == m[1] && n[3] == m[2] && n[4] == m[3] && n[5] == m[4])
+                i[1]++;
+            else if (n[0] == m[0] && n[1] == m[1] && n[2] == m[2] && n[3] == m[3])
+                i[2]++;
+            else if (n[1] == m[0] && n[2] == m[1] && n[3] == m[2] && n[4] == m[3])
+                i[2]++;
+            else if (n[2] == m[0] && n[3] == m[1] && n[4] == m[2] && n[5] == m[3])
+                i[2]++;
+        } while (c != EOF);
+        if (i[0] != 0 || i[1] != 0 || i[2] != 0)
         {
-            printf(" Digite sua jogada:");
-            scanf("%i %i %i %i %i", &m[0], &m[1], &m[2], &m[3], &m[4]);
-            do
-            {
-                c = fscanf(pFile, "%i %i %i %i %i %i %i", &n[6], &n[0], &n[1], &n[2], &n[3], &n[4], &n[5]);
-                if (n[0] == m[0] && n[1] == m[1] && n[2] == m[2] && n[3] == m[3] && n[4] == m[4])
-                    i++;
-                else if (n[1] == m[0] && n[2] == m[1] && n[3] == m[2] && n[4] == m[3] && n[5] == m[4])
-                    i++;
-            } while (c != EOF);
-            if (i > 0)
-                printf("\n Voce ganhou a quina %i vezes!\n", i);
+            if (i[0] > 0)
+                printf("\n Voce ganhou a mega sena %i vezes!\n", i[0]);
             else
-                printf("\n Voce nao ganhou nenhum jogo\n");
-        }
-        else if (mod == 2)
-        {
-            printf(" Digite sua jogada:");
-            scanf("%i %i %i %i", &m[0], &m[1], &m[2], &m[3]);
-            do
-            {
-                c = fscanf(pFile, "%i %i %i %i %i %i %i", &n[6], &n[0], &n[1], &n[2], &n[3], &n[4], &n[5]);
-                if (n[0] == m[0] && n[1] == m[1] && n[2] == m[2] && n[3] == m[3])
-                    i++;
-                else if (n[1] == m[0] && n[2] == m[1] && n[3] == m[2] && n[4] == m[3])
-                    i++;
-                else if (n[2] == m[0] && n[3] == m[1] && n[4] == m[2] && n[5] == m[3])
-                    i++;
-            } while (c != EOF);
-            if (i > 0)
-                printf("\n Voce ganhou a quadra %i vezes!\n", i);
+                printf("\n Voce nao ganhou a mega sena\n");
+            if (i[1] > 0)
+                printf("\n Voce ganhou a quina %i vezes!\n", i[1]);
             else
-                printf("\n Voce nao ganhou nenhum jogo\n");
+                printf("\n Voce nao ganhou a quina\n");
+            if (i[2] > 0)
+                printf("\n Voce ganhou a quadra %i vezes!\n", i[2]);
+            else
+                printf("\n Voce nao ganhou a quadra\n");
         }
         else
-            printf(" input invalido\n");
+            printf("\n Voce nao ganhou nenhum jogo\n");
         fclose(pFile);
     }
 }
